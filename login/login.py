6e0d9c537ce7e0a02b1e5c208614d1c49b6c61ca
evanjdenny <143561921+evanjdenny@users.noreply.github.com>
2023-09-19 20:06:20 -0400

    Update login.py

--- login/login.py
+++ login/login.py
@@ -1,27 +1,44 @@
-profiles_db = [('EvanDenny', 'evandenny')]
+import hashlib
+profiles_db = []
+
+class Database:
+    def __init__(self):
+        self.profiles = []
+
+    def add_profile(self, username: str, password: str):
+        pwd = hashlib.sha256(password)
+        self.profiles.append((username, pwd.hexdigest()))
+
+    def remove_profile(self, username: str):
+        for i in range(len(self.profiles)):
+            if self.profiles[i][0] == username:
+                self.profiles.remove(self.profiles[i])
 
 class Login:
-    database = []
-    username = None
-    password = None
-    success = False
+    database = Database()
 
-    def __init__(self, database: list):
-        self.database = database
-    
-    def __call__(self):
-        while not self.success:
-            self.username = input("USERNAME: ")
-            self.password = input("PASSWORD: ")
-            self.check_db()
-            if not self.success:
-                print('Login UNSUCCESSFUL! Please try again. ')
-        print('Login SUCCESSFUL!')
+    def __init__(self):
+        self.username = None
+        self.password = None
+        self.logged_in = False
 
-    def check_db(self):
-        for item in self.database:
-            if item[0] == self.username and item[1] == self.password:
-                self.success = True
+    def add_profile(self, username: str, password: str):
+        self.database.add_profile(username, password)
 
-login = Login(profiles_db)
+    def login(self):
+        self.username = input('USERNAME:')
+        self.password = input('PASSWORD:')
+        for item in database.profiles:
+            if item[0] == self.username:
+                pwd = hashlib.sha256(self.password)
+                if pwd.hexdigest() == item[1]:
+                    self.logged_in = True
+                    print(f'LOGIN SUCCESSFUL! You are now signed in as {self.username}')
+                    return
+        if not self.logged_in:
+            print('LOGIN FAILED!')
+
+login = Login()
+login.add_profile('EvanDenny', 'ejdenny1997')
+login.login()
 login()
